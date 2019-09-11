2e7df5bc5a522ce862a2393c0d93c55bf1ab8489
viper1298 <techguy8527@gmail.com>
2019-09-11 09:37:36 +0530

    commit

--- BookStoreWebsite/src/com/bookstore/controller/admin/CreateUserServlet.java
+++ BookStoreWebsite/src/com/bookstore/controller/admin/CreateUserServlet.java
@@ -14,7 +14,7 @@ import com.bookstore.service.UserServices;
 public class CreateUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
+	//new code
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("email");
 		response.getWriter().println("Email:" + email);
